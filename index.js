1cf69cae962e0f622bb1c5efdf7c28e1fbf6eddf
atamanahmet <ahmtataman@proton.me>
2024-09-27 15:11:04 +0300

    temp JSON for req

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import axios from "axios";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
 let tempParams = "";
 var stuff;
+let tempdata=fs.readFileSync("tempdata.json");
+// console.log(JSON.parse(tempdata)); 
+// JSON.parse(tempdata)
+
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -31,12 +36,21 @@ app.post("/search", async (req, res) => {
   
     const response = await axios.get(url);
     stuff = response.data.docs;
+    // var asd="asdasd";
+    fs.writeFile("tempdata.json", JSON.stringify(stuff), (err)=>{err && console.log(err);})
   } catch (error) {
     console.log(error.message);
   }
   res.locals.stuff = stuff;
+  // if()
   res.render("index.ejs", stuff);
 });
+
+app.post("/viewCard", (req,res) => {
+  console.log(req.body.name);
+  locals.data=req.body.name;
+  res.render("viewCard.ejs", data)
+})
 
 app.listen(port, (req, res) => {
   console.log("Listenin on port : " + port);
