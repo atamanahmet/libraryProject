b629d9b8125ec4784ea76eca153fd5d089462789
atamanahmet <ahmtataman@proton.me>
2024-09-27 13:20:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+let tempParams = "";
+var stuff;
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/search", async (req, res) => {
+  console.log(req.body.searchParams);
+  tempParams = "";
+  const searchParamsArray = req.body.searchParams.split(" ");
+  console.log(searchParamsArray);
+  for (let i = 0; i < searchParamsArray.length; i++) {
+    if (i == searchParamsArray.length - 1) {
+      tempParams = tempParams + searchParamsArray[i];
+    } else {
+      tempParams = tempParams + searchParamsArray[i] + "+";
+    }
+  }
+
+  const url = "https://openlibrary.org/search.json?q=" + tempParams;
+  try {
+    console.log(url);
+    const response = await axios.get(url);
+    stuff = response.data.docs;
+  } catch (error) {
+    console.log(error.message);
+  }
+  res.locals.stuff = stuff;
+  console.log(stuff);
+  res.render("index.ejs", stuff);
+});
+
+app.listen(port, (req, res) => {
+  console.log("listenin on port : " + port);
+});
