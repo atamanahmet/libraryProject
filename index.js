f891d9f450857626fb567e69391bca1dfc839427
atamanahmet <ahmtataman@proton.me>
2024-09-27 16:24:00 +0300

    modified:   index.js 	modified:   tempdata.json 	modified:   views/index.ejs 	modified:   views/viewCard.ejs 	new file:   yaz.json

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const app = express();
 const port = 3000;
 let tempParams = "";
 var stuff;
-let tempdata=fs.readFileSync("tempdata.json");
-// console.log(JSON.parse(tempdata)); 
+var tempRead = fs.readFileSync("tempdata.json", "utf-8");
+if (tempRead) {
+  var tempdata = JSON.parse(tempRead);
+}
+// console.log(tempdata[0].author_name[0]);
+// var str = "ahmet ataman"
+// console.log(str.includes("ata"));
+
+// let tempdata = JSON.parse(tempRead);
+// console.log(tempRead);
+// console.log(JSON.parse(tempdata));
 // JSON.parse(tempdata)
 
 app.use(express.static("public"));
@@ -19,10 +28,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/search", async (req, res) => {
-  console.log(req.body.searchParams);
+  // console.log(req.body.searchParams);
   tempParams = "";
   const searchParamsArray = req.body.searchParams.split(" ");
-  console.log(searchParamsArray);
+
+  // console.log(searchParamsArray);
+
   for (let i = 0; i < searchParamsArray.length; i++) {
     if (i == searchParamsArray.length - 1) {
       tempParams = tempParams + searchParamsArray[i];
@@ -33,24 +44,34 @@ app.post("/search", async (req, res) => {
 
   const url = "https://openlibrary.org/search.json?q=" + tempParams;
   try {
-  
     const response = await axios.get(url);
     stuff = response.data.docs;
     // var asd="asdasd";
-    fs.writeFile("tempdata.json", JSON.stringify(stuff), (err)=>{err && console.log(err);})
+    fs.writeFile("tempdata.json", JSON.stringify(stuff), (err) => {
+      err && console.log(err);
+    });
   } catch (error) {
     console.log(error.message);
   }
+  if (tempRead) {
+    console.log("Write Succesful");
+  }
   res.locals.stuff = stuff;
-  // if()
-  res.render("index.ejs", stuff);
+ 
+    res.render("index.ejs", stuff);
+
 });
 
-app.post("/viewCard", (req,res) => {
-  console.log(req.body.name);
-  locals.data=req.body.name;
-  res.render("viewCard.ejs", data)
-})
+app.post("/viewCard", (req, res) => {
+  console.log(req.body);
+  var asd= JSON.stringify(req.body);
+  fs.writeFile("yaz.json", asd, (err)=>{
+    console.log(err);
+  })
+  // res.locals.stuff = JSON.parse(req.body);
+  console.log(stuff);
+  res.render("viewCard.ejs", stuff);
+});
 
 app.listen(port, (req, res) => {
   console.log("Listenin on port : " + port);
